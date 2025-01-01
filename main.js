66d3aa994297e8486f99ccfe22eab09e0ab16993
Khurram Saleem Butt <khurram@dns-1.ttc.local>
2025-01-01 18:00:15 +0500

    First Commit

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("*", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("*", (req, res) => res.send("<h1>Hello World!This is my first commit</h1>"));
 
 app.listen(80, () => console.log("App is listening"));
